a233f55a07d4a2662a283067b33891ab70c559ae
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2023-08-11 16:36:10 +0100

    netlify build

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -18,6 +18,7 @@ const ProductListing = () => {
 
   useEffect(() => {
     fetchProducts();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   console.log("Products :", products);
